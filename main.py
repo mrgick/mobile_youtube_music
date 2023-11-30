ec779b2c83b54953b2ca981e4d60883c372568ac
mrgick <mrgickcool@gmail.com>
2023-11-30 16:59:58 +0300

    add buttons to toolbar

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.boxlayout import BoxLayout
 from search_screen import SearchScreen
+from playlists_screen import PlaylistsScreen
+from player_screen import PlayerScreen
 
-class PlayerScreen(MDScreen):
-    pass
+from kivy.uix.screenmanager import ScreenManager, NoTransition
 
 
-class PlaylistsScreen(MDScreen):
-    pass
+
+
+
 
 class YoutubeMusicApp(MDApp):
     dialog = None
@@ -27,12 +29,15 @@ class YoutubeMusicApp(MDApp):
         Builder.load_file("main.kv")
         self.theme_cls.primary_palette = "Blue"
         self.theme_cls.theme_style = "Light"
-        sm = ScreenManager()
+        sm = ScreenManager(transition=NoTransition())
         sm.add_widget(SearchScreen(name="search_screen"))
-        # sm.add_widget(PlaylistsScreen(name="playlists_screen"))
-        # sm.add_widget(PlayerScreen(name="player_screen"))
+        sm.add_widget(PlaylistsScreen(name="playlists_screen"))
+        sm.add_widget(PlayerScreen(name="player_screen"))
         self.sm = sm
         return sm
+
+    def switch_screen(self, screen_name:str):
+        self.sm.current = screen_name
 
 
 if __name__ == "__main__":
